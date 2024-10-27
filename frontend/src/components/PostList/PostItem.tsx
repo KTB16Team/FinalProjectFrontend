14d04fc13c2315a6a46c654102c3a2ea3c00d78f
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-10-27 22:51:00 +0900

    refactor: PostItem 이미지 추가 및 css

--- frontend/src/components/PostList/PostItem.tsx
+++ frontend/src/components/PostList/PostItem.tsx
@@ -1,5 +1,7 @@
 import {PostForm} from "@/types/postForm.ts";
 import {Link} from "react-router-dom";
+import CommentLogo from "@/assets/imgs/Comment.svg?react";
+import LikeLogo from "@/assets/imgs/Like.svg?react";
 
 interface PostItemProps {
   post: PostForm;
@@ -7,18 +9,20 @@ interface PostItemProps {
 
 export default function PostItem({post}: PostItemProps) {
   return (
-    <Link to={`/posts/${post.id}`}>
-      <div>
-        <div>{post.id}</div>
-        <div>{post.title}</div>
-        <div>{post.content}</div>
-        <div>
-          <span>{post.createdAt}</span>
-          <span>{post.views}</span>
-          <span>{post.likes}</span>
-          <span>{post.comments}</span>
+    <div className="bg-white rounded-2xl mb-2">
+      <Link to={`/posts/${post.id}`}>
+        <div className="text-left">
+          <div>{post.id}</div>
+          <div>{post.title}</div>
+          <div>{post.content}</div>
+          <div className="flex flex-row">
+            <div>{post.createdAt}</div>
+            <div>조회 {post.views}</div>
+            <div className="flex flex-row"><LikeLogo/>{post.likes}</div>
+            <div className="flex flex-row"><CommentLogo/>{post.comments}</div>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
