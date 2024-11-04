5f325d8c15c89b6ce9eb2f5f4bd7a9ec4edccbbe
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-11-04 15:11:15 +0900

    feat: 프론트엔드 포트 변경

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
